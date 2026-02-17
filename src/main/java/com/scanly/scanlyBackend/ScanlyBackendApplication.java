c5c6645012aa288c75e66f5b1bb0b17ce6740dd5
Kalousu <kevinbecker848@gmail.com>
2026-02-17 22:44:48 +0100

    initial commit

---
+++ src/main/java/com/scanly/scanlyBackend/ScanlyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.scanly.scanlyBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScanlyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScanlyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scanly/scanlyBackend/ScanlyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scanly.scanlyBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScanlyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
