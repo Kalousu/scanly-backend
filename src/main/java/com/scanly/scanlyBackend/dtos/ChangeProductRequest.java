6a841504f241d834f8f85492937383235aed0ac0
Lukas Groening <113829864+yb981@users.noreply.github.com>
2026-04-11 17:53:58 +0200

    chore: removed more unused imports

--- src/main/java/com/scanly/scanlyBackend/dtos/ChangeProductRequest.java
+++ src/main/java/com/scanly/scanlyBackend/dtos/ChangeProductRequest.java
@@ -1,6 +1,5 @@
 package com.scanly.scanlyBackend.dtos;
 
-import com.scanly.scanlyBackend.models.enums.PricingType;
 import com.scanly.scanlyBackend.models.enums.ProductCategory;
 
 import java.math.BigDecimal;
